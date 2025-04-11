5baa38fc3d2c11f1d9f9e2c2ebfc6fb2a2b25da5
irenecual <cualterosirene@gmail.com>
2025-04-11 15:19:35 -0700

    Added Roles and Uder Seeder, Added AdminMiddleware & Edit RedirectAuthenticated, added Role in Models

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+
 use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Http\Request;
@@ -21,7 +22,11 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                $role = Role::Where('id', auth()->user()->role_id)->first();
+                if ($role){
+                    return redirect($role->url); 
+                }
+                
             }
         }
 
